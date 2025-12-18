d747b1811d345f7def2c2416abe9149a0edca499
Wealthometer <wealthhajoh87@gmail.com>
2025-12-18 23:04:00 +0100

    Create AppLayout.vue

---
+++ src/components/AppLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="min-h-screen flex">
+    <Sidebar />
+    <main class="flex-1 lg:ml-64">
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<script>
+import Sidebar from "./Sidebar.vue"
+
+export default {
+  name: "AppLayout",
+  components: {
+    Sidebar,
+  },
+}
+</script>
